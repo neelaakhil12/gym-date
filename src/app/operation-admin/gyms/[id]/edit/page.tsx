47ef8fe43ee9e4ec77f7ea0ea41fe03f9276158e
neelaakhil12 <neelaakhil12@users.noreply.github.com>
2026-05-08 11:19:11 +0530

    Add: Staff gym create and edit pages

---
+++ src/app/operation-admin/gyms/[id]/edit/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import AdminEditGymPage from "@/app/admin/gyms/[id]/edit/page";
+
+export default function StaffEditGymPage() {
+  return <AdminEditGymPage />;
+}
---
+++ src/app/operation-admin/gyms/create/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import AdminCreateGymPage from "@/app/admin/gyms/create/page";
+
+export default function StaffCreateGymPage() {
+  return <AdminCreateGymPage />;
+}
